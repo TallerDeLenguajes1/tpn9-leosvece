e2b02a8012e9fad89317eb0245a7b4cee6693519
Leo <programa0.1@outlook.com>
2020-06-17 01:50:56 -0300

    tp9

---
+++ tp9/tp9/Class1.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Linq;
+
+namespace helpers
+{
+    static class soporteParaConf
+    { 
+        public static void CrearArchivoDeConfiguracion(string nombre, string directorio)
+        {
+            
+            if (!(Directory.Exists(directorio + @"\trabajo")))
+            {
+                Directory.CreateDirectory(directorio + @"\trabajo");
+            }
+            if(!(File.Exists(directorio + @"\trabajo\" + nombre + ".dat")))
+            {
+                File.Create(directorio + @"\trabajo\" + nombre + ".dat");
+            }
+
+        }
+
+        public static string LeerConfiguracion(string nombre, string directorio)
+        {
+            string nota;
+            if (File.Exists(directorio + @"\trabajo\" + nombre + ".dat"))
+            {
+                
+                string a = directorio + @"\trabajo\" + nombre + ".dat";
+                FileInfo archivo = new FileInfo(a);
+                nota = "nombre: " +archivo.Name + "\n";
+                nota += "extencion: " + archivo.Extension + "\n";
+                nota += "fecha de creacion: " + archivo.CreationTime + "\n";
+                long bits = (archivo.Length / 2014);
+                nota += "tamaño: " + Convert.ToString(bits);
+                return nota;
+            }
+            else
+            {
+                return "no existe el archivo seleccionado";
+            }            
+            
+        }
+
+        static Dictionary<char, String> claveMorse = new Dictionary<char, String>()
+        {
+            {'a' , ".-"},
+            {'b' , "-..."},
+            {'c' , "-.-."},
+            {'d' , "-.."},
+            {'e' , "."},
+            {'f' , "..-."},
+            {'g' , "--."},
+            {'h' , "...."},
+            {'i' , ".."},
+            {'j' , ".---"},
+            {'k' , "-.-"},
+            {'l' , ".-.."},
+            {'m' , "--"},
+            {'n' , "-."},
+            {'o' , "---"},
+            {'p' , ".--."},
+            {'q' , "--.-"},
+            {'r' , ".-."},
+            {'s' , "..."},
+            {'t' , "-"},
+            {'u' , "..-"},
+            {'v' , "...-"},
+            {'w' , ".--"},
+            {'x' , "-..-"},
+            {'y' , "-.--"},
+            {'z' , "--.."},
+            {'0' , "-----"},
+            {'1' , ".----"},
+            {'2' , "..---"},
+            {'3' , "...--"},
+            {'4' , "....-"},
+            {'5' , "....."},
+            {'6' , "-...."},
+            {'7' , "--..."},
+            {'8' , "---.."},
+            {'9' , "----."},
+        };
+
+        static Dictionary<string, char> caracteres = new Dictionary<string, char>()
+        {
+            {".-",'a'},
+            { "-...",'b'},
+            {"-.-.",'c'},
+            {"-..",'d'},
+            { ".",'e'},
+            {"..-.", 'f' },
+            {"--.",'g' },
+            {"....",'h' },
+            { "..",'i' },
+            { ".---",'j'},
+            { "-.-",'k'},
+            {".-..",'l' },
+            {"--",'m' },
+            { "-.",'n'},
+            { "---",'o'},
+            { ".--.",'p'},
+            {"--.-", 'q'},
+            {".-.",'r'},
+            {"...",'s' },
+            { "-",'t'},
+            {"..-",'u'},
+            {"...-",'v'},
+            { ".--", 'w'},
+            { "-..-",'x'},
+            {"-.--", 'y'},
+            {"--..",'z'},
+            {"-----",'0' },
+            {".----",'1'},
+            {"..---",'2'},
+            { "...--",'3'},
+            { "....-",'4'},
+            {"....." ,'5'},
+            {"-....",'6' },
+            { "--...",'7'},
+            { "---..",'8'},
+            {"----.",'9'},
+        };
+        public static string morseAtexto(string texto)
+        {
+            string traduccion = null;
+
+            foreach (char caracter in texto)
+            {
+                if(caracter == ' ')
+                {
+                    traduccion += "/";
+                }
+                else
+                {
+                    traduccion += claveMorse[caracter] + " ";
+                }
+            }
+
+            return traduccion;
+        }
+
+        public static string textoAmorse(string texto)
+        {
+            string traduccion = null, aux = null;
+
+            foreach (char caracter in texto)
+            {
+                if (caracter == '/')
+                {
+                    traduccion += " ";
+                }
+                else if(caracter != ' ')
+                {
+                    aux += caracter;              
+                }
+                else
+                {
+                    traduccion += caracteres[aux];
+                    aux = null;
+                }
+                
+            }
+            traduccion += caracteres[aux];
+
+            return traduccion;
+        }
+
+    }
+}
---
+++ tp9/tp9/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using helpers;
+
+namespace tp9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*  string directorio = @"\taller y lenguaje\tp9\trabajo";
+             String rutaOrigen;
+
+             Console.WriteLine("Ingrese el nombre del archivo que desea crear");
+             string nombre = Console.ReadLine();
+             Console.WriteLine("rellene el archivo");
+             string instruccion = Console.ReadLine();
+             soporteParaConf.CrearArchivoDeConfiguracion(nombre, directorio);
+
+             if (File.Exists(directorio + @"\trabajo\" + nombre + ".dat"))
+             {
+
+                 File.WriteAllText(directorio + @"\trabajo\" + nombre + ".dat", "1010101011");
+                 string texto = File.ReadAllText(directorio + @"\trabajo\\" + nombre + ".dat");
+                 Console.WriteLine("contenido del archivo \n" + texto);
+                 string lectura = soporteParaConf.LeerConfiguracion(nombre, directorio);
+                 Console.WriteLine(lectura);
+             }
+             else
+             {
+                 Console.WriteLine("no existe la ruta del archivo {0}", directorio);
+             }
+             //////////////////////////////punto b///////////////
+             rutaOrigen = System.Environment.CurrentDirectory;
+             List<string> listadoArchivos = Directory.GetFiles(rutaOrigen).ToList();
+             foreach (string archivos in listadoArchivos)
+             {
+                 Console.WriteLine(archivos);
+                 FileInfo file = new FileInfo(archivos);
+                 if ((file.Extension == ".mp3") || (file.Extension == ".txt"))
+                 {
+                     string nuevoDestino = directorio + file.Name;
+                     File.Move(archivos, nuevoDestino);
+                 }
+             }
+             */
+
+
+            Console.WriteLine("traductor a clave morse");
+            Console.WriteLine("ingrese el texto que quiere traducir");
+
+            string texto = Console.ReadLine();
+            texto = texto.ToLower();
+
+            Console.WriteLine("su traduccion: ");
+            texto = soporteParaConf.morseAtexto(texto);
+            Console.WriteLine(texto);
+
+            Console.WriteLine("traductor de clave morse");
+            Console.WriteLine("ingrese el texto que quiere traducir");
+
+            string clave = Console.ReadLine();
+            
+            Console.WriteLine("su traduccion: ");
+            clave = soporteParaConf.textoAmorse(clave);
+            Console.WriteLine(clave);
+
+
+
+
+        }
+    }
+}
+
